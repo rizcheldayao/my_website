759cb3e8551b72e98898c01321baaa477935206a
Rizchel Dayao <rdayao@us.ibm.com>
2016-11-01 18:13:15 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/';
+
+app.use(express.static(__dirname + '/public'));
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+app.use("/",router);
+
+// app.use("*",function(req,res){
+//   res.sendFile(path + "404.html");
+// });
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
---
+++ public/js/index.js
@@ -0,0 +1,3 @@
+function redirect() {
+  window.location.href = 'https://github.com/rizcheldayao/api-project';
+}
