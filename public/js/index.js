87bffec50eab6bc36e5786947093436d7a98f002
Rizchel Dayao <rdayao@us.ibm.com>
2016-11-08 15:32:42 -0600

    Pushing final website

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,25 @@
-function redirect() {
-  window.location.href = 'https://github.com/rizcheldayao/api-project';
+function getSite(val) {
+  switch (val) {
+    case 'blog':
+      window.open('https://rizcheldayaoblog.wordpress.com/');
+      break;
+    case 'website':
+      window.open('https://github.com/rizcheldayao/personal-website');
+      break;
+    case 'babel':
+      window.open('https://github.com/rizcheldayao/babel_setup');
+      break;
+    case 'react':
+      window.open('http://codepen.io/rizcheldayao/pen/jVPPPR?editors=1010');
+      break;
+    case 'api':
+      window.open('https://github.com/rizcheldayao/api-project');
+      break;
+    case 'resume':
+      window.open('../views/Rizchel_Resume.html');
+      break;
+
+    default:
+      return;
+  }
 }
