cc64dfe1bf0541fcb268a57ecaa35c00b2d1c852
Rizchel Dayao <rdayao@us.ibm.com>
2016-11-10 15:15:16 -0600

    Added github pages

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var deploy      = require('gulp-gh-pages');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
@@ -9,4 +10,9 @@ gulp.task('styles', function() {
 
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
+});
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
 });
