443598ba3f0e3afc7e808fa6802c57b4cfc90e6d
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-02-01 15:19:39 -0700

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 function getSite(val) {
   switch (val) {
     case 'blog':
-      window.open('https://rizcheldayaoblog.wordpress.com/');
+      window.open('hhttps://medium.com/@rizchel.a.dayao');
       break;
     case 'website':
       window.open('https://github.com/rizcheldayao/personal-website');
