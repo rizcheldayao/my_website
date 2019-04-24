69fdd5b88411335f6d72bb261a29b905f1894293
Rizchel Wood <rizchel.a.dayao@gmail.com>
2019-04-24 13:47:09 -0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
 function getSite(val) {
   switch (val) {
     case 'blog':
-      window.open('https://medium.com/@rizchel.a.dayao');
+      window.open('https://medium.com/@rizchelwood');
       break;
     case 'website':
-      window.open('https://github.com/rizcheldayao/personal-website');
+      window.open('https://github.com/rizchelwood/personal-website');
       break;
     case 'javascript':
       window.open('https://twobecomewood.com/');
       break;
     case 'react':
-      window.open('https://github.com/rizcheldayao/react-location-api-example');
+      window.open('https://github.com/rizchelwood/react-location-api-example');
       break;
     case 'api':
-      window.open('https://github.com/rizcheldayao/anagramsAPI');
+      window.open('https://github.com/rizchelwood/anagramsAPI');
       break;
     case 'react-redux':
       window.open('https://github.com/IBM/deploy-react-kubernetes');
